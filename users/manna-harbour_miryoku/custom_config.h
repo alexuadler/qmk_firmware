5776b0a39dddb36edd74742f54c0bac80634f4ed
alexuadler <alexuadler@users.noreply.github.com>
2023-04-30 00:08:09 -0400

    changed around the mouse layer

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -7,6 +7,10 @@
 
 // #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 
+#define ROTATIONAL_TRANSFORM_ANGLE -25
+
+#define CHARYBDIS_AUTO_SNIPING_ON_LAYER 5
+
 // #define AUTO_MOUSE_DEFAULT_LAYER 5
 
 #define MIRYOKU_LAYER_BASE \
@@ -16,7 +20,7 @@ BU_Z,              ALGR_T(KC_X),      KC_C,              KC_V,              KC_B
 U_NP,              U_NP,              FUN_DEL,           NUM_BSP,         SYM_ENT,           MO_TAB,            NAV_SPC,  LT(U_MEDIA,KC_ESC),U_NP,              U_NP
 
 #define MIRYOKU_LAYER_MOUSE \
-TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_NU,              U_NU,           U_NU,           U_NU,           U_NU,           \
-KC_LGUI,         KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,                   KC_WH_U,              U_NU,           U_NU,           U_NU,           U_NU,           \
-U_NA,              KC_ALGR,           TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  U_NA,              KC_WH_D,             KC_BTN1,           KC_BTN2,           KC_BTN3,         U_NU,             \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              TD(U_TD_U_BASE),           TD(U_TD_U_MOUSE),           U_NP,           U_NP,              U_NP
+TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              S_D_MOD,              DPI_MOD,           U_NU,           U_NU,           U_NU,           \
+KC_LGUI,         KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,                   KC_WH_U,              U_NU,           SNIPING,           U_NU,           U_NU,           \
+U_NA,              KC_ALGR,           TD(U_TD_U_SYM),    TD(U_TD_U_MOUSE),  U_NA,              U_NU,             KC_BTN1,           KC_BTN2,           DRGSCRL,         U_NU,             \
+U_NP,              U_NP,              SNIPING,              KC_BTN1,           KC_BTN2,              TD(U_TD_U_BASE),           TD(U_TD_U_MOUSE),           U_NP,           U_NP,              U_NP
